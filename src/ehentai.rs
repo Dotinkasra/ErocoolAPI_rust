8ca5feb18c1b484094dcb6027aae689ad79df941
Dotinkasra <tyekepontv@gmail.com>
2023-05-25 06:58:11 +0900

    add : get viewer links function

--- src/ehentai.rs
+++ src/ehentai.rs
@@ -1,11 +1,8 @@
 use scraper::Html;
 use scraper::Selector;
+use regex::Regex;
 
 pub fn get_manga_name(html: &Html) -> String {
-    let imglinks = get_all_imglink(html);
-    for imglink in imglinks {
-        println!("{}", imglink);
-    }
     let selector_str: &str = "#gj";
     let selector: Selector = Selector::parse(selector_str).unwrap();
     for element in html.select(&selector) {
@@ -14,8 +11,7 @@ pub fn get_manga_name(html: &Html) -> String {
     return "Untitled".to_string();
 }
 
-
-pub fn get_all_imglink(html: &Html) -> Vec<String> {
+fn get_all_imglink(html: &Html) -> Vec<String> {
     let gdt_selector: &str = "#gdt div";
     let selector: Selector = Selector::parse(gdt_selector).unwrap();
     let mut imglinks: Vec<String> = vec![];
@@ -30,3 +26,38 @@ pub fn get_all_imglink(html: &Html) -> Vec<String> {
     }
     return imglinks;
 }
+
+fn get_viewer_links(html: &Html)  -> Vec<String>{
+    let mut viewer_links: Vec<String> = vec![];
+
+    let selector_td: Selector = Selector::parse("body > div:nth-child(10) > table > tbody > tr td").unwrap();
+    let selector_a: Selector = Selector::parse("a").unwrap();
+    
+    let element_tds: &mut scraper::html::Select = &mut html.select(&selector_td);
+    let element_lastpage: scraper::ElementRef = element_tds.rev().nth(1).unwrap();
+
+    let last_page_a: &mut scraper::element_ref::Select = &mut element_lastpage.select(&selector_a);
+    let i = last_page_a.next().unwrap();
+    let url = i.value().attr("href").unwrap();
+
+    let re = Regex::new(r"https://e-hentai.org/g/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)/\?p=(\d+)").unwrap();
+
+    // Some(変数名) の形であれば
+    if let Some(captures) = re.captures(&url) {
+        let content_link = String::from("https://e-hentai.org/g/") + &captures[1] + "/" + &captures[2] + "/";
+        let template = content_link.clone();
+
+        viewer_links.push(content_link);
+
+        let last_page_num = &captures[3].parse::<u8>().unwrap();
+
+        for i in 1..=*last_page_num {
+            println!("{}", i);
+            let url = template.to_string() + "p=" + &i.to_string();
+            viewer_links.push(url);
+        }
+    }
+    return viewer_links;
+
+}
+
