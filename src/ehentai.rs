b6c49d42cac8e526506c65115be4ab057e79528e
Dotinkasra <tyekepontv@gmail.com>
2023-05-26 02:51:34 +0900

    change : print to log

--- src/ehentai.rs
+++ src/ehentai.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use log::{info, warn, error, trace};
+
 use scraper::Html;
 use scraper::Selector;
 use regex::Regex;
@@ -6,19 +9,22 @@ use super::Manga;
 
 pub async fn get_ehentai(html: &Html) -> Manga {
     let manga_name: String = get_manga_name(html);
+    info!("【Name】:{}", &manga_name);
+
     let viewer_page_links: Vec<String> = get_viewer_links(html);
     let img_links: &mut Vec<String> = &mut vec![];
     
     for viewer in viewer_page_links {
+        info!("【viewer】:{}", &viewer);
         let response = reqwest::get(viewer).await.unwrap().text().await.unwrap();
         let html: Html = Html::parse_document(&response);
         let all_imglink = get_all_imglink(&html);
         for img in all_imglink {
+            info!("【img】:{}", &img);
             img_links.push(single_page_scraper(&img).await);
         }
     }
-
-    return Manga{title: manga_name, pages: img_links.to_vec(), total_pages_num: img_links.len() as u8};
+    return Manga{title: manga_name, pages: img_links.to_vec()};
 }
 
 fn get_manga_name(html: &Html) -> String {
@@ -31,7 +37,7 @@ fn get_manga_name(html: &Html) -> String {
     return "Untitled".to_string();
 }
 
-fn get_all_imglink(html: &Html) -> Vec<String> {
+fn get_all_imglink(html: &Html) -> HashSet<String> {
     let gdt_selector: &str = "#gdt div";
     let selector: Selector = Selector::parse(gdt_selector).unwrap();
     let mut imglinks: Vec<String> = vec![];
@@ -43,7 +49,8 @@ fn get_all_imglink(html: &Html) -> Vec<String> {
             imglinks.push(link.to_string());
         }
     }
-    return imglinks;
+    let uniq_imglinks: HashSet<String> = imglinks.into_iter().collect();
+    return uniq_imglinks;
 }
 
 fn get_viewer_links(html: &Html)  -> Vec<String>{
@@ -71,24 +78,21 @@ fn get_viewer_links(html: &Html)  -> Vec<String>{
         let last_page_num = &captures[3].parse::<u8>().unwrap();
 
         for i in 1..=*last_page_num {
-            let url = template.to_string() + "p=" + &i.to_string();
-            viewer_links.push(url);
+            let numbered_url = template.to_string() + "p=" + &i.to_string();
+            viewer_links.push(numbered_url);
         }
+    } else {
+        viewer_links.push(url.to_string());
     }
     return viewer_links;
 
 }
 
 async fn single_page_scraper(url: &str) -> String {
-    //let client = reqwest::Client::new();
     let response = reqwest::get(url).await.unwrap().text().await.unwrap();
-
     let html: Html = Html::parse_document(&response);
-
     let selector = Selector::parse("#img").unwrap();
-
     let img_select = &mut html.select(&selector);
-
     let img_src = img_select.nth(0)
                             .unwrap()
                             .value()
